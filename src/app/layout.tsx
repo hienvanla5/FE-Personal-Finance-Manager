4e0868ed8ee5ef58c49d9b5b096841d44228f70f
hienvanla5 <hienvla05@gmail.com>
2026-06-26 21:58:46 +0700

    fix avatar bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} suppressHydrationWarning
     >
       <body className="min-h-full">
         <ThemeInit />
